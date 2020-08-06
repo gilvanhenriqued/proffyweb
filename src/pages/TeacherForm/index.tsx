9986042cfe9c271fb992a0aa97d2890c1f5d0634
Gilvan Henrique <gilvanh.15@gmail.com>
2020-08-06 12:11:48 -0300

    input component and teacher form page

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,12 +1,51 @@
 import React from 'react';
 import Header from '../../components/Header';
+import Input from '../../components/Input';
+
+import warningIcon from '../../assets/images/icons/warning.svg';
+
+import './styles.css'
 
 function TeacherForm() {
   return (
     <div id="page-teacher-form" className="container">
       <Header
         title="Que incrível que você quer dar aulas!"
+        description="O primeiro passo é preencher esse formulário de inscrição"
       />
+
+      <main>
+        <fieldset>
+          <legend>Seus dados</legend>
+
+          <Input name="name" label="Nome Completo" />
+          <Input name="avatar" label="Avatar" />
+          <Input name="whatsapp" label="WhatsApp" />
+        </fieldset>
+
+        <fieldset>
+          <legend>Sobre a aula</legend>
+
+          <Input name="materia" label="Matéria" />
+          <Input name="cost" label="Custo de sua aula por hora" />
+        </fieldset>
+
+        <fieldset>
+          <legend>Horários Disponíveis</legend>
+
+          <Input name="name" label="Nome Completo" />
+          <Input name="avatar" label="Avatar" />
+        </fieldset>
+
+        <footer>
+          <p>
+            <img src={warningIcon} alt="Aviso Importante"/>
+            Importante! <br />
+            Preencha todos os dados.
+          </p>
+          <button type="button">Salvar o Cadastro</button>
+        </footer>
+      </main>
     </div>
   );
 }
