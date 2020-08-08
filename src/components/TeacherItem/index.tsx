ea8552a95e7aeb97e41dc515b0ba4c6996ab302e
Gilvan Henrique <gilvanh.15@gmail.com>
2020-08-07 21:30:19 -0300

    total connection updated at landing page

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
 export interface Teacher {
   id: number;
@@ -19,6 +20,14 @@ interface TeacherItemProps {
 }
 
 const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  const text = `Olá ${teacher.name}! Quero marcar uma aula...`
+
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id,
+    })
+  }
+
   return (
     <article className="teacher-item">
       <header>
@@ -38,10 +47,13 @@ const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
           Preço/hora
           <strong>R$ {teacher.cost}</strong>
         </p>
-        <button>
+        <a 
+          onClick={createNewConnection}
+          target="_blank"
+          href={`https://wa.me/${teacher.whatsapp}?text=${text}`}>
           <img src={whatsappIcon} alt="Whatsapp do professor" />
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   )
